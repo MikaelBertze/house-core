62b8cb313fd55ac017b4c3ac8d24866aaf260eba
Mikael Bertze <mikael@bertze.com>
2022-07-03 22:24:54 +0200

    fixing slow query

--- dotnet/house-api/houseServices/temperatureService.cs
+++ dotnet/house-api/houseServices/temperatureService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using HouseCore.Contracts;
 using HouseCore.HouseService.Mongo;
 using MongoDB.Driver;
@@ -15,15 +16,22 @@ namespace HouseCore.HouseService
 
         public IEnumerable<TemperatureInfo> GetInfo()
         {
-            var now = DateTime.Now;
+            var today = DateTime.Today.ToUniversalTime();
+            //var now = DateTime.Now;
+            
+            //var utcnow = now.ToUniversalTime();
+            
             IMongoDatabase db = _mongoClient.GetDatabase("house");
             var temperatures = db.GetCollection<Temperature>("temperature");
-            var sensors = temperatures.Distinct(x => x.SensorId, x=> x.TimeStamp > now - TimeSpan.FromDays(1)).ToList();
+            var data = temperatures.Find(x=> x.TimeStamp >= today - TimeSpan.FromDays(1)).ToList();
+            var sensors = data.Select(d => d.SensorId).Distinct();
 
             foreach(var sensor in sensors){
-                
-                var doc = temperatures.Find(x => x.SensorId == sensor).SortByDescending(x => x.TimeStamp).FirstOrDefault();
-                yield return new TemperatureInfo { Sensor = sensor, Temperature = doc.Value, ReportTime = doc.TimeStamp.ToLocalTime()};
+                var temps = data.Where(x => x.SensorId == sensor).OrderByDescending(x => x.TimeStamp).ToList();
+                var doc = temps.FirstOrDefault();
+                var minValue = temps.Where(x => x.TimeStamp >= today).Min(x => x.Value);
+                var maxValue = temps.Where(x => x.TimeStamp >= today).Max(x => x.Value);
+                yield return new TemperatureInfo { Sensor = sensor, Temperature = doc.Value, ReportTime = doc.TimeStamp.ToLocalTime(), DayMax = maxValue, DayMin = minValue };
             }
         }
     }
