2360e0a02e080981a55b73929dc76454a942a87d
Mikael Bertze <mikael@bertze.com>
2022-07-03 22:25:44 +0200

    adds min/max temperature

--- dotnet/house-api/Contracts/TemperatureInfo.cs
+++ dotnet/house-api/Contracts/TemperatureInfo.cs
@@ -5,5 +5,9 @@ namespace HouseCore.Contracts{
         public DateTime ReportTime {get;set;}
         public string Sensor {get;set;}
         public double Temperature {get;set;}
+
+        public double DayMax {get;set;}
+        public double DayMin {get;set;}
+
     }
 }
